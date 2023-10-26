394b486a92fb13e80b5ed8297e1e63cac48f9350
Daniel Ekström <daniel.ekstrm08@gmail.com>
2023-10-26 21:27:47 +0200

    new logging feature

--- app/udpsocket.py
+++ app/udpsocket.py
@@ -1,5 +1,4 @@
 import socket
-import pandas
 import time
 import influxdb_client
 from influxdb_client.client.write_api import SYNCHRONOUS
@@ -8,11 +7,30 @@ import dotenv
 import logging
 
 
-logging.basicConfig(filename='app.log', level=logging.ERROR,
-                    format='%(asctime)s [%(levelname)s]: %(message)s')
+def setup_logging():
+
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+
+    error_handler = logging.FileHandler('error.log')
+    info_handler = logging.FileHandler('info.log')
+
+    error_handler.setLevel(logging.ERROR)
+    info_handler.setLevel(logging.INFO)
+
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    error_handler.setFormatter(formatter)
+    info_handler.setFormatter(formatter)
+
+    logger.addHandler(error_handler)
+    logger.addHandler(info_handler)
+
+    return logger
 
 
-def main():
+def main(logging=logging):
     datadict = {
         "320008014E9B00DF": "ElPanna",
         "FB0008014E9B01DF": "LVP",
@@ -29,14 +47,15 @@ def main():
         "F500000CB53D6528": "V Vatten",
     }
     print("STARTING")
-
     dotenv.load_dotenv()
+
     try:
         BUCKET = os.environ.get('bucket')
         ORG = os.environ.get('org')
         TOKEN = os.environ.get('token')
         UDP_IP = os.environ.get('UDP_IP')
         UDP_PORT = int(os.environ.get('UDP_PORT'))
+        MY_IP = os.environ.get('MY_IP')
 
         # Store the URL of your InfluxDB instance
         URL = os.environ.get("url")
@@ -64,6 +83,8 @@ def main():
         return
 
     logging.info("Socket created")
+    logging.info(f"UDP_IP: {UDP_IP} UDP_PORT: {UDP_PORT}")
+    logging.info(sock)
     logging.info("STARTED")
     results = {}
 
@@ -72,14 +93,18 @@ def main():
             data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
             data = data.decode().split("&")
             logging.info(f"Recieved data: {data}")
-            for item in data:
-                id_, value = item.split('=')
-                name = datadict.get(id_, id_).replace(" ", "-")
-                results[name] = value
-                write_api.write(bucket=BUCKET, org=ORG, record=influxdb_client.Point(
-                    "_measurement").tag("location", name).field("temperature", float(value)))
-            logging.info(f"Data written to InfluxDB: {results}")
-            results.clear()
+            logging.info(f"Recieved data from: {addr}")
+            if (addr[0] == MY_IP):
+                for item in data:
+                    id_, value = item.split('=')
+                    name = datadict.get(id_, id_).replace(" ", "-")
+                    results[name] = value
+                    write_api.write(bucket=BUCKET, org=ORG, record=influxdb_client.Point(
+                        "_measurement").tag("location", name).field("temperature", float(value)))
+                logging.info(f"Data written to InfluxDB: {results}")
+                results.clear()
+            else:
+                logging.info(f"Recieved data from unknown IP: {addr}")
         except Exception as e:
             logging.error(f"Error data: {e}")
 
@@ -87,7 +112,8 @@ def main():
 if __name__ == "__main__":
     while True:
         try:
-            main()
+            logger = setup_logging()
+            main(logging=logger)
         except Exception as e:
             logging.error(f"Main function crashed: {e}")
             time.sleep(5)
